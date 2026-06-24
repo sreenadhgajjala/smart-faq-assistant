8057656621aa9133fb8bd0cd4628cc6078d63fda
Sreenadh Gajjala <23kb1a3316@nbkrist.org>
2026-06-24 19:35:46 +0530

    Fix utils.py and remove NLTK code

--- utils.py
+++ utils.py
@@ -9,22 +9,3 @@ def preprocess(text):
 def load_faqs():
     with open("faqs.json", "r", encoding="utf-8") as f:
         return json.load(f)
-
-    tokens = [
-        word
-        for word in tokens
-        if word not in stop_words
-        and word not in string.punctuation
-    ]
-
-    return " ".join(tokens)
-
-def load_faqs():
-
-    with open(
-        "faqs.json",
-        "r",
-        encoding="utf-8"
-    ) as f:
-
-        return json.load(f)
